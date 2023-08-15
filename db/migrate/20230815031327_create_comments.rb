d7d023f61e2f258401ff73c288706bef55bee348
Sergio Peralta <seperalta88@gmail.com>
2023-08-15 13:30:49 -0300

    Update migrations

--- db/migrate/20230815031327_create_comments.rb
+++ db/migrate/20230815031327_create_comments.rb
@@ -1,14 +1,10 @@
 class CreateComments < ActiveRecord::Migration[7.0]
   def change
     create_table :comments do |t|
-      t.integer :post_id
-      t.integer :author_id
       t.text :text
 
       t.timestamps
     end
 
-    add_index :comments, :post_id
-    add_index :comments, :author_id
   end
 end
