a902a4f093f59abdabf827d548857bd28cf62018
Sergio Peralta <seperalta88@gmail.com>
2023-08-15 11:39:53 -0300

    Add user model

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,27 +3,14 @@ class Post < ApplicationRecord
   has_many :comments
   has_many :likes
 
-
   def liked_by?(user)
     likes.where(user:).exists?
   end
 
   private
 
-  def comments_count=(value)
-    self[:comments_count] = value
-  end
-
-  def likes_count=(value)
-    self[:likes_count] = value
-  end
-
-  def update_likes_count
-    self.likes_count = likes.count
-  end
-
-  def update_comments_count
-    self.comments_count = comments.count
+  def update_post_counter
+    author.increment!(:posts_counter)
   end
 
   def recent_comments
