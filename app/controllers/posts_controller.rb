181236374cefa32dcc86bdd21572b5118a782e6a
Sergio Peralta <seperalta88@gmail.com>
2023-08-22 06:32:40 -0300

    Add post_show view

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,5 +4,8 @@ class PostsController < ApplicationController
     @posts = Post.where(author_id: params[:user_id])
   end
 
-  def show; end
+  def show
+    @post = Post.find_by(id: params[:id])
+    @comments = Comment.where(post_id: params[:id])
+  end
 end
